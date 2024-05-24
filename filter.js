24efade5d028a2100a82c9a80f15061659a9db80
Januar <adhinugroho623@gmail.com>
2024-05-24 20:11:33 +0700

    update

--- filter.js
+++ filter.js
@@ -17,3 +17,24 @@ function closeFilter() {
     document.getElementById('overlay-filter').classList.remove('active');
     document.getElementById('filterContainer').classList.remove('active');
 }
+
+
+document.getElementById('filterButton2').addEventListener('click', function() {
+    if (!document.getElementById('filterContainer2').classList.contains('active')) {
+        // Jika filter container belum aktif, aktifkan overlay dan filter container
+        document.getElementById('overlay-filter2').classList.add('active');
+        document.getElementById('filterContainer2').classList.add('active');
+    } else {
+        // Jika filter container sudah aktif, tutup overlay dan filter container
+        closeFilter2();
+    }
+});
+
+document.getElementById('overlay-filter2').addEventListener('click', function() {
+    closeFilter2();
+});
+
+function closeFilter2() {
+    document.getElementById('overlay-filter2').classList.remove('active');
+    document.getElementById('filterContainer2').classList.remove('active');
+}
