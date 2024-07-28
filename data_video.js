58af0c50cab5ad7ca6e6c83bcc7589443ae60f52
Januar <adhinugroho623@gmail.com>
2024-07-28 23:36:37 +0700

    update data_video.js

--- data_video.js
+++ data_video.js
@@ -6015,7 +6015,7 @@
                 "gendre2": "Adventure",
                 "gendre3": "Comedy",
                 "description": "Franchise animasi global terbesar dalam sejarah kini memulai babak baru dengan Gru dan Lucy dan putri angkat mereka —Margo, Edith dan Agnes—menyambut anggota baru di keluarga Gru, Gru Jr., yang berniat menyiksa ayahnya. Gru menghadapi musuh baru dalam diri Maxime Le Mal dan pacarnya yang femme fatale, Valentina, dan keluarganya terpaksa melarikan diri. English – The biggest global animated franchise in history now begins a new chapter as Gru and Lucy and their girls —Margo, Edith and Agnes—welcome a new member to the Gru family, Gru Jr., who is intent on tormenting his dad. Gru faces a new nemesis in Maxime Le Mal and his femme fatale girlfriend Valentina, and the family is forced to go on the run.",
-                "bgimg": "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQamTyCKfIsY2OGNJqujUPABLZ8yN97prWd1vvkOgpTkqpQxOvQ0G_h0_jhuhpWtWO5_M3__-uSJVeOD5Qf",
+                "bgimg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuJUECVQzF8BLl-sqIB91xyKC3Vq_lt9Qf0A&usqp=CAU",
                 "trailer": "https://www.youtube.com/embed/LtNYaH61dXY",
                 "duration": "1 jam 34m",
 "episodes": [
